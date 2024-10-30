7a220b1bd1cdc668ad9b15044294e2bb5433f8fc
Yzakiel1390 <yzakiel1390@gmail.com>
2024-10-29 21:25:33 -0300

    PFA - Distância Máxima do Player diminuída

--- 1Ano/JogoFimDeAno/duet.py
+++ 1Ano/JogoFimDeAno/duet.py
@@ -57,7 +57,7 @@ class Player:
     def update(self, screen: pg.Surface, point: tuple[int, int], key: pg.key.ScancodeWrapper) -> None:
         if key[pg.K_LSHIFT] and self.distance > 0:
             self.distance -= 5
-        elif key[pg.K_SPACE] and self.distance < SCREEN_SIZE[0]//2 or self.distance < 100 and not key[pg.K_LSHIFT]:
+        elif key[pg.K_SPACE] and self.distance < SCREEN_SIZE[0]//3 or self.distance < 100 and not key[pg.K_LSHIFT]:
             self.distance += 5
         elif self.distance > 100 and not key[pg.K_SPACE]:
             self.distance -= 5
