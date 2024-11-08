cff4b83a1aa7de988cd1f95084d7688fc5827752
Yzakiel1390 <yzakiel1390@gmail.com>
2024-11-07 22:05:29 -0300

    Respostas colocadas em Listas 2

--- 1Ano/Estruturas/listas2.py
+++ 1Ano/Estruturas/listas2.py
@@ -6,14 +6,14 @@ match choice:
     case '1':
         L = [5, 0, 3, 8, 9, 4, 1, 2]
 
-        print(L[0:])
-        print(L[:5])
-        print(L[2:7])
-        print(L[3:4])
-        print(L[-7:-3])
-        print(L[:15])
-        print(L[::2])
-        print(L[1::3])
-        print(L[-2::-1])
+        print(L[0:])     # [5, 0, 3, 8, 9, 4, 1, 2]
+        print(L[:5])     # [5, 0, 3, 8, 9]
+        print(L[2:7])    # [3, 8, 9, 4, 1]
+        print(L[3:4])    # [8]
+        print(L[-7:-3])  # [0, 3, 8, 9]
+        print(L[:15])    # [5, 0, 3, 8, 9, 4, 1, 2]
+        print(L[::2])    # [5, 3, 9, 1]
+        print(L[1::3])   # [0, 9, 2]
+        print(L[-2::-1]) # [1, 4, 9, 8, 3, 0, 5]
     case _:
         print("Opção Inválida")
