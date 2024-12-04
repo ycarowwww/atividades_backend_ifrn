02d3cf2b88007a7af904ba5cd6e5e5ef167cd3e8
Yzakiel1390 <yzakiel1390@gmail.com>
2024-12-04 20:43:10 -0300

    PFA - Pequena otimização com a classe Player

--- 1Ano/JogoFimDeAno/duet.py
+++ 1Ano/JogoFimDeAno/duet.py
@@ -107,52 +107,6 @@ def circles_intersection(screen: pg.Surface, player1: tuple[pg.Surface, tuple[in
 
     screen.blit(surf1, player1_topleft)
 
-class Player:
-    def __init__(self, position: list[int], color: tuple[int, int, int], radius: int, angle: int = 0, speed: float = 5) -> None:
-        self.position = position
-        self.color = color
-        self.radius = radius
-        self.angle = angle
-        self.speed = speed
-        self.positions_track: list[list[int]] = []
-        self.distance = 100
-        self.color_track = adjust_brightness(self.color, 0.5)
-    
-    def update(self, screen: pg.Surface, point: tuple[int, int], key: pg.key.ScancodeWrapper) -> None:
-        if (key[pg.K_LSHIFT] or key[pg.K_DOWN]) and self.distance > 0:
-            self.distance -= self.speed
-        elif (key[pg.K_SPACE] or key[pg.K_UP]) and self.distance < SCREEN_SIZE[0]//3 or self.distance < 100 and not (key[pg.K_LSHIFT] or key[pg.K_DOWN]):
-            self.distance += self.speed
-        elif self.distance > 100 and not (key[pg.K_SPACE] or key[pg.K_UP]):
-            self.distance -= self.speed
-        if key[pg.K_a] or key[pg.K_LEFT]: self.angle -= self.speed
-        if key[pg.K_d] or key[pg.K_RIGHT]: self.angle += self.speed
-        self.angle %= 360
-
-        self.rotate_to_center(point)
-        self.draw(screen)
-    
-    def draw(self, screen: pg.Surface) -> None:
-        track_length: int = len(self.positions_track)
-        for line in range(track_length-1, 0, -1):
-            line_width: int = int(-2 * self.radius / track_length * line + 2 * self.radius)
-            line_color: list[int] = [-color / track_length * line + color for color in list(self.color_track)]
-            pg.draw.line(screen, line_color, self.positions_track[line-1], self.positions_track[line], line_width)
-            pg.draw.circle(screen, line_color, self.positions_track[line], line_width//2)
-        
-        pg.draw.circle(screen, self.color, self.position, self.radius)
-
-    def rotate_to_center(self, point: tuple[int, int]) -> None:
-        self.position[0] = int(self.distance * cos(radians(self.angle)) + point[0])
-        self.position[1] = int(self.distance * sin(radians(self.angle)) + point[1])
-
-        self.positions_track.insert(0, self.position.copy()) # Usar uma Queue depois
-        if len(self.positions_track) >= 150 / self.speed:
-            self.positions_track.pop(-1)
-
-        for line in range(1, len(self.positions_track)):
-            self.positions_track[line][1] += self.speed
-
 class Obstacle:
     def __init__(self, position: list[int], color: tuple[int, int, int], size: list[int]) -> None:
         self.position = position
@@ -180,15 +134,78 @@ class Obstacle:
         
         pg.draw.rect(screen, self.color, self.hitbox_rect)
 
-def game() -> None:
-    player1: Player = Player(list(SCREEN_CENTER), COLORS["BLUE"], 20, speed=PLAYER_ROTATION_VELOCITY)
-    player2: Player = Player(list(SCREEN_CENTER), COLORS["RED"], 20, 180, speed=PLAYER_ROTATION_VELOCITY)
+class Player:
+    def __init__(self, center: tuple[int, int], colors_circles: list[tuple[int, int, int]], border_color: tuple[int, int, int], radius: int, distance: int = 100, speed: float = 5) -> None:
+        self.center = center
+        self.colors = colors_circles
+        self.radius = radius
+        self.amount = len(self.colors)
+        self.positions = [[0, 0] for _ in range(self.amount)]
+        self.angle = 0
+        self.d_angle = 360 // self.amount
+        self.speed = speed
+        self.distance = distance
+        self.show_border = False
+        self.border_size = 5
+        self.border_color = border_color
+        # Falta as linhas das posições, colisão e intersecção
+        # self.positions_track: list[list[int]] = []
+        # self.colors_track = [adjust_brightness(self.colors[i]) for i in range(len(self.colors) - 1)]
+    
+    def update(self) -> None:
+        key: pg.key.ScancodeWrapper = pg.key.get_pressed()
+        
+        if key[pg.K_LSHIFT] and self.distance > 0:
+            self.distance -= self.speed
+        elif key[pg.K_SPACE] and self.distance < SCREEN_SIZE[0]//3 or self.distance < 100 and not key[pg.K_LSHIFT]:
+            self.distance += self.speed
+        elif self.distance > 100 and not key[pg.K_SPACE]:
+            self.distance -= self.speed
 
-    surf_player1: pg.Surface = pg.Surface((player1.radius * 2, player1.radius * 2), pg.SRCALPHA)
-    surf_player2: pg.Surface = pg.Surface((player2.radius * 2, player2.radius * 2), pg.SRCALPHA)
+        if key[pg.K_a]: self.angle -= self.speed
+        if key[pg.K_d]: self.angle += self.speed
+        self.angle %= 360
+
+        self.rotate_to_center()
+    
+    def draw(self, screen: pg.Surface) -> None:
+        if self.show_border:
+            pg.draw.circle(screen, self.border_color, self.center, self.distance, self.border_size)
+        
+        # track_length: int = len(self.positions_track)
+        # for line in range(track_length-1, 0, -1):
+        #     line_width: int = int(-2 * self.radius / track_length * line + 2 * self.radius)
+        #     line_color: list[int] = [-color / track_length * line + color for color in list(self.color_track)]
+        #     pg.draw.line(screen, line_color, self.positions_track[line-1], self.positions_track[line], line_width)
+        #     pg.draw.circle(screen, line_color, self.positions_track[line], line_width//2)
+        
+        for i in range(self.amount):
+            pg.draw.circle(screen, self.colors[i], self.positions[i], self.radius)
+
+    def rotate_to_center(self) -> None:
+        for i in range(self.amount):
+            self.positions[i][0] = int(self.distance * cos(radians(self.angle) + radians(self.d_angle * i)) + self.center[0])
+            self.positions[i][1] = int(self.distance * sin(radians(self.angle) + radians(self.d_angle * i)) + self.center[1])
+
+        # self.positions_track.insert(0, pos.copy()) # Usar uma Queue depois
+        # if len(self.positions_track) >= 150 / self.speed:
+        #     self.positions_track.pop(-1)
+
+        # for line in range(1, len(self.positions_track)):
+        #     self.positions_track[line][1] += self.speed
+    
+    def toggle_border(self) -> None:
+        self.show_border = not self.show_border
+
+def game() -> None:
+    player: Player = Player(list(SCREEN_CENTER), [COLORS["BLUE"], COLORS["RED"]], COLORS["GRAY"], 20, speed=PLAYER_ROTATION_VELOCITY)
+    # player1: Player = Player(list(SCREEN_CENTER), COLORS["BLUE"], 20, speed=PLAYER_ROTATION_VELOCITY)
+    # player2: Player = Player(list(SCREEN_CENTER), COLORS["RED"], 20, 180, speed=PLAYER_ROTATION_VELOCITY)
+
+    # surf_player1: pg.Surface = pg.Surface((player1.radius * 2, player1.radius * 2), pg.SRCALPHA)
+    # surf_player2: pg.Surface = pg.Surface((player2.radius * 2, player2.radius * 2), pg.SRCALPHA)
 
     paused: bool = False
-    show_borders: bool = False
     pause_button_rect: pg.Rect = pg.Rect(0, 0, 50, 50)
     pause_button_rect.topright = (SCREEN_SIZE[0] - 10, 10)
     pause_button_triangle: list[tuple[int, int]] = (pause_button_rect.topleft, pause_button_rect.bottomleft, (pause_button_rect.right, pause_button_rect.centery))
@@ -218,7 +235,7 @@ def game() -> None:
                 if event.key == pg.K_ESCAPE:
                     paused = not paused
                 if event.key == pg.K_b:
-                    show_borders = not show_borders
+                    player.toggle_border()
 
             if event.type == pg.MOUSEBUTTONDOWN:
                 mx, my = event.pos
@@ -231,18 +248,16 @@ def game() -> None:
         clock.tick(FPS)
         screen.fill(COLORS["BLACK"])
 
-        if show_borders:
-            pg.draw.circle(screen, COLORS["GRAY"], SCREEN_CENTER, player1.distance, 5)
-
         if paused:
             pg.draw.polygon(screen, COLORS["WHITE"], pause_button_triangle)
             pg.draw.polygon(screen, COLORS["WHITE"], (return_menu_button.topright, return_menu_button.bottomright, (return_menu_button.left, return_menu_button.centery)))
 
-            player1.draw(screen)
-            player2.draw(screen)
+            player.draw(screen)
+            # player1.draw(screen)
+            # player2.draw(screen)
 
-            if collision_circles(player1.position, player2.position, player1.radius, player2.radius):
-                circles_intersection(screen, (surf_player1, player1.position, player1.radius), (surf_player2, player2.position, player2.radius), COLORS["WHITE"])
+            # if collision_circles(player1.position, player2.position, player1.radius, player2.radius):
+            #     circles_intersection(screen, (surf_player1, player1.position, player1.radius), (surf_player2, player2.position, player2.radius), COLORS["WHITE"])
 
             for rect in obstacles_list:
                 rect.draw(screen)
@@ -256,18 +271,18 @@ def game() -> None:
             for rt in pause_button_rects:
                 pg.draw.rect(screen, COLORS["WHITE"], rt)
 
-        player1.update(screen, SCREEN_CENTER, key)
-        player2.update(screen, SCREEN_CENTER, key)
+        player.update()
+        player.draw(screen)
 
-        if collision_circles(player1.position, player2.position, player1.radius, player2.radius):
-            circles_intersection(screen, (surf_player1, player1.position, player1.radius), (surf_player2, player2.position, player2.radius), COLORS["WHITE"])
+        # if collision_circles(player1.position, player2.position, player1.radius, player2.radius):
+        #     circles_intersection(screen, (surf_player1, player1.position, player1.radius), (surf_player2, player2.position, player2.radius), COLORS["WHITE"])
 
         for rect in obstacles_list:
             rect.movement_bottom()
             rect.draw(screen)
             # Check Collision with players
-            if collision_circle_rect(player1.position, player1.radius, rect.hitbox_rect) or collision_circle_rect(player2.position, player2.radius, rect.hitbox_rect):
-                punctuation = 0
+            # if collision_circle_rect(player1.position, player1.radius, rect.hitbox_rect) or collision_circle_rect(player2.position, player2.radius, rect.hitbox_rect):
+            #     punctuation = 0
 
         if obstacles_list[0].position[1] >= SCREEN_SIZE[1]:
             obstacles_list.pop(0)
